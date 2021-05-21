855f6b4a6c183d3c484e548c1c480011d9b01097
Eka-G <ekaglazkovaa@yandex.ru>
2021-05-21 03:10:19 +0300

    build: create start configuration

---
+++ match-match-game/.eslintrc.js
@@ -0,0 +1,27 @@
+module.exports = {
+  env: {
+    es6: true,
+  },
+  parserOptions: {
+    ecmaVersion: 2017,
+  },
+  extends: ['prettier'],
+  overrides: [
+    {
+      files: ['*.ts'],
+      env: {
+        browser: true,
+        es2021: true,
+      },
+      extends: ['airbnb-typescript/base', 'prettier'],
+      parserOptions: {
+        ecmaVersion: 12,
+        sourceType: 'module',
+        project: './tsconfig.json',
+      },
+      settings: {
+        'import/ignore': [/.html$/],
+      },
+    },
+  ],
+};
---
+++ match-match-game/webpack.config.js
@@ -0,0 +1,86 @@
+// Generated using webpack-cli https://github.com/webpack/webpack-cli
+
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const ESLintWebpackPlugin = require('eslint-webpack-plugin');
+
+const isProduction = process.env.NODE_ENV == 'production';
+
+const stylesHandler = isProduction ? MiniCssExtractPlugin.loader : 'style-loader';
+
+const config = {
+  entry: {
+    app: './src/index.ts',
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name].[contenthash].js',
+    assetModuleFilename: 'assets/[name][hash][ext]',
+  },
+  devServer: {
+    open: true,
+    host: 'localhost',
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+    }),
+    // new MiniCssExtractPlugin({
+    // 	filename: "[name].[contenthash].css",
+    // }),
+    new CopyPlugin({
+      patterns: [{ from: './public' }],
+    }),
+    new CleanWebpackPlugin({
+      cleanStaleWebpackAssets: false,
+    }),
+    new ESLintWebpackPlugin({
+      extensions: ['ts', 'js'],
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.(ts|tsx)$/i,
+        loader: 'ts-loader',
+        exclude: ['/node_modules/'],
+      },
+      {
+        test: /\.css$/i,
+        use: [stylesHandler, 'css-loader'],
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
+      },
+      {
+        test: /\.(ico|svg|png|jpg|gif)$/i,
+        type: 'asset/resource',
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+  },
+};
+
+module.exports = () => {
+  if (isProduction) {
+    config.mode = 'production';
+
+    config.plugins.push(
+      new MiniCssExtractPlugin({
+        filename: '[name].[contenthash].css',
+      }),
+    );
+  } else {
+    config.mode = 'development';
+    config.devtool = 'inline-source-map';
+
+    config.plugins.push(new MiniCssExtractPlugin());
+  }
+  return config;
+};
