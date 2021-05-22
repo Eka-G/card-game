96c550f4afa96878208d7b2c680968529ae68568
Eka-G <ekaglazkovaa@yandex.ru>
2021-05-22 04:35:47 +0300

    feat: implement start component structure (app, base-component)

--- match-match-game/src/index.ts
+++ match-match-game/src/index.ts
@@ -1,1 +1,10 @@
-import './styles.scss';
+import './scss/styles.scss';
+import App from './app';
+
+window.onload = () => {
+  const appElem = document.getElementById('app');
+
+  if (!appElem) throw Error('App root element not found');
+
+  new App(document.body);
+};
