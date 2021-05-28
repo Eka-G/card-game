fe1463f73723e7523523d8d4ed77fcf2c70c6ce6
Eka-G <ekaglazkovaa@yandex.ru>
2021-05-28 21:09:12 +0300

    fix(delay): fix eslint export error

--- match-match-game/src/shared/delay.ts
+++ match-match-game/src/shared/delay.ts
@@ -1,5 +1,4 @@
-// eslint-disable-next-line import/prefer-default-export
-export function delay(timeout: number): Promise<void> {
+export default function delay(timeout: number): Promise<void> {
   return new Promise((resolve) => {
     setTimeout(resolve, timeout);
   });
